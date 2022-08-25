65fdde47a85c033f09c3e31185a47cc120d314ee
Ishan Siddiqui <ishansiddiqui123@gmail.com>
2022-08-25 11:28:47 +0530

    changed 3.0.sh .. should work

--- jenkins-3.0.sh
+++ jenkins-3.0.sh
@@ -2,13 +2,17 @@
 sudo yum update -y
 # check Java, install - 
 java -version
-sudo yum install java-1.8.0
+sudo amazon-linux-extras install java-openjdk11 -y
 sudo /usr/sbin/alternatives --config java
 # get repo..
-sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat/jenkins.repo
+sudo wget -O https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 
-sudo yum install jenkins
-sudo service jenkins start
+sudo yum upgrade
+
+sudo yum install jenkins -y
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+sudo systemctl status jenkins
 
 
