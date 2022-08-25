961e535220371c35610fa2d6e8756524aae82ccc
Ishan Siddiqui <ishansiddiqui123@gmail.com>
2022-08-25 11:58:32 +0530

    fixed commands

--- jenkins-3.0.sh
+++ jenkins-3.0.sh
@@ -5,7 +5,7 @@ java -version
 sudo amazon-linux-extras install java-openjdk11 -y
 sudo /usr/sbin/alternatives --config java
 # get repo..
-sudo wget -O https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 
 sudo yum upgrade
