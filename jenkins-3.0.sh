023ec2f3631ddb7eae9127698879dca8242e6c74
Ishan Siddiqui <ishansiddiqui123@gmail.com>
2022-08-25 11:20:51 +0530

    m

--- jenkins-3.0.sh
+++ jenkins-3.0.sh
@@ -5,7 +5,7 @@ java -version
 sudo yum install java-1.8.0
 sudo /usr/sbin/alternatives --config java
 # get repo..
-sudo wget -O /etc/yum.repos.d/jenkins.repo \ https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 
 sudo yum install jenkins
