8fcf30fdbea1ef03c57a74bd4528ac7da04b4e94
Ishan Siddiqui <ishansiddiqui123@gmail.com>
2022-08-25 11:15:58 +0530

    new script - 3.0

---
+++ jenkins-3.0.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+sudo yum update -y
+# check Java, install - 
+java -version
+sudo yum install java-1.8.0
+sudo /usr/sbin/alternatives --config java
+# get repo..
+sudo wget -O /etc/yum.repos.d/jenkins.repo \ https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+
+sudo yum install jenkins
+sudo service jenkins start
+
+
