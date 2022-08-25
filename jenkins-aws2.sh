f65dc5ec8b82275602085917cc9a3c705c4e3045
Ishan Siddiqui <ishansiddiqui123@gmail.com>
2022-08-25 10:54:25 +0530

    added jenkins-aws2.sh file..

---
+++ jenkins-aws2.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+sudo yum update -y
+
+sudo yum install java-1.8.0
+sudo yum remove java-1.7.0-openjdk
+
+sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
+
+sudo rpm — import https://pkg.jenkins.io/redhat/jenkins.io.key
+
+sudo yum install jenkins -y
+
+sudo service jenkins start
