4cd57503c26b51811ecf28d160f0d8b69e2b2cc0
Ishan Siddiqui <ishansiddiqui123@gmail.com>
2022-10-11 19:47:17 +0530

    added sudo

--- gradle.sh
+++ gradle.sh
@@ -5,13 +5,13 @@ gradle_version=”4.4-rc-5″
 #Your Custom Installation Path
 
 install_target_path=”/opt/gradle”
-wget -N https://services.gradle.org/distributions/gradle-${gradle_version}-all.zip
-mkdir -p ${install_target_path}
-unzip gradle-${gradle_version}-all.zip
-mv gradle-${gradle_version} ${install_target_path}/
-ln -sfn gradle-${gradle_version} ${install_target_path}/latest
-chown -R ec2-user:ec2-user ${install_target_path}
-printf “export GRADLE_HOME=${install_target_path}/latest\nexport PATH=\$PATH:\$GRADLE_HOME/bin” > /etc/profile.d/gradle.sh
+sudo wget -N https://services.gradle.org/distributions/gradle-${gradle_version}-all.zip
+sudo mkdir -p ${install_target_path}
+sudo unzip gradle-${gradle_version}-all.zip
+sudo mv gradle-${gradle_version} ${install_target_path}/
+sudo ln -sfn gradle-${gradle_version} ${install_target_path}/latest
+sudo chown -R ec2-user:ec2-user ${install_target_path}
+sudo printf “export GRADLE_HOME=${install_target_path}/latest\nexport PATH=\$PATH:\$GRADLE_HOME/bin” > /etc/profile.d/gradle.sh
 . /etc/profile.d/gradle.sh
 # check installation
 gradle -v
