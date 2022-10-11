7cd1b28f98cfa28901c6fc4a84e03b64687f53a8
Ishan Siddiqui <ishansiddiqui123@gmail.com>
2022-10-11 20:03:00 +0530

    changed wget and mkdir steps

--- gradle.sh
+++ gradle.sh
@@ -5,9 +5,9 @@ gradle_version=”7.5.1″
 #Your Custom Installation Path
 
 install_target_path=”/opt/gradle”
-sudo wget -N https://services.gradle.org/distributions/gradle-${gradle_version}-bin.zip
 sudo mkdir /opt/gradle
-ls /opt/gradle
+sudo wget -N https://services.gradle.org/distributions/gradle-${gradle_version}-bin.zip
+# ls /opt/gradle
 # sudo unzip gradle-${gradle_version}-all.zip
 # ls /opt/gradle/gradle-7.5.1
 
