a8fc10be3ad88d99df7399d4a1f99835115d44f1
Ishan Siddiqui <ishansiddiqui123@gmail.com>
2022-10-11 19:40:37 +0530

    changed unzip to tar

--- gradle.sh
+++ gradle.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sudo mkdir /opt/gradle
 wget -c https://services.gradle.org/distributions/gradle-7.5.1-bin.zip
-sudo unzip -d /opt/gradle/gradle-7.5.1-bin.zip
+sudo tar xvf -d /opt/gradle/gradle-7.5.1-bin.zip
 export PATH=$PATH:/opt/gradle/gradle-7.5.1/bin
 # check installation
 gradle -v
