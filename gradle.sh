0777cc27ae17f28f47406fa4999fa0187007f3d4
Ishan Siddiqui <ishansiddiqui123@gmail.com>
2022-10-11 20:00:17 +0530

    made everything else priv..

--- gradle.sh
+++ gradle.sh
@@ -8,11 +8,11 @@ install_target_path=”/opt/gradle”
 sudo wget -N https://services.gradle.org/distributions/gradle-${gradle_version}-bin.zip
 sudo mkdir /opt/gradle
 ls /opt/gradle
-sudo unzip gradle-${gradle_version}-all.zip
-ls /opt/gradle/gradle-7.5.1
+# sudo unzip gradle-${gradle_version}-all.zip
+# ls /opt/gradle/gradle-7.5.1
 
-sudo chown -R ec2-user:ec2-user ${install_target_path}
-export PATH=$PATH:/opt/gradle/gradle-7.5.1/bin
+# sudo chown -R ec2-user:ec2-user ${install_target_path}
+# export PATH=$PATH:/opt/gradle/gradle-7.5.1/bin
 
 # check installation
 gradle -v
